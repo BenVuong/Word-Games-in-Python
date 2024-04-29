c921a08dad586eedfc9f1337c845b4290337008c
Ben G Vuong <ben.vuong2000@gmail.com>
2024-04-29 16:41:09 -0500

    added user input

--- hangman.py
+++ hangman.py
@@ -6,16 +6,25 @@ for i in range(len(answer)):
 for i in range(len(unknownWord)):
     print(unknownWord[i], end=" ")
 
+def checkLetter(insertChar, target):
+    
+    if(target.find(insertChar)!=-1):
+        locatArray = []
+    
+        while(target.find(insertChar)!=-1):
+            locatArray.append(target.find(insertChar))
+            target=target.replace(insertChar,"*"*len(insertChar),1)    
 
-insertChar = "l"
-locatArray = []
-while(answer.find(insertChar)!=-1):
-    locatArray.append(answer.find(insertChar))
-    answer=answer.replace(insertChar,"*"*len(insertChar),1)
-
-for i in range(len(locatArray)):
-    unknownWord[locatArray[i]]=insertChar
+        for i in range(len(locatArray)):  
+            unknownWord[locatArray[i]]=insertChar
+        print("\n")
+        for i in range(len(unknownWord)):
+            print(unknownWord[i], end=" ")
+    else:
+        print("Letter not in word")    
+   
 print("\n")
-for i in range(len(unknownWord)):
-    print(unknownWord[i], end=" ")
+input = input('Guess a letter that might be in the word:')[0]
+checkLetter(input, answer)
+
 
