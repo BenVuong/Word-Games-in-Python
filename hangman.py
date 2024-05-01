09ea9ec66ce7768cd7ceefe657b1a13399061eb8
Ben G Vuong <ben.vuong2000@gmail.com>
2024-05-01 17:55:33 -0500

    added a text file of words where at the begining the hangman game will choose a random word from it as the answer

--- hangman.py
+++ hangman.py
@@ -1,11 +1,10 @@
-answer = "hello"
+from random import randint
+wordList = []
 turns = 8
 unknownWord = []
-for i in range(len(answer)):
-    unknownWord.append("-")
+listOfWords = []
 
-for i in range(len(unknownWord)):
-    print(unknownWord[i], end=" ")
+
 
 def checkLetter(insertChar, target):
     
@@ -25,19 +24,43 @@ def checkLetter(insertChar, target):
     else:
         print("Letter not in word")    
         print("\n")
+        for i in range(len(unknownWord)):
+            print(unknownWord[i], end=" ")
+        print("\n")
 
 
+f = open("wordList.txt", "r")
+for x in f:
+    listOfWords.append(x)
+f.close
+answerIndex = randint(0,len(listOfWords)-1)
+answer=listOfWords[answerIndex]
+
+
+print("Welcome to Hangman")
+print("A random word will be choosen and you will have 8 turns to guess a letter that belongs in that word")
+print("On the 8th turn you will have to type in the entire word")
+for i in range(len(answer)):
+    unknownWord.append("-")
+for i in range(len(unknownWord)):
+    print(unknownWord[i], end=" ")
 for i in range(turns):   
-    print("\n Turn: " + str(i) + "\n")
-    userInput = input('Guess a letter that might be in the word:')[0]
-    checkLetter(userInput, answer)
-    final = "".join(unknownWord)
-    if(final == answer):
-        print("\n You win" )
-        break
-    if i == turns-1:
-        print("You lose")
-        print("The word was: " + answer)
+    print("\n Turn: " + str(i+1) + "\n")
+    if(i == turns-1):
+        userInput = input("Guess the word:")
+        if(userInput == answer):
+            print("You win")
+        else:
+            print("You lose")
+            print("The word was: " + answer)
+    else:
+        userInput = input('Guess a letter that might be in the word:')[0]
+        checkLetter(userInput, answer)
+        final = "".join(unknownWord)
+        if(final == answer):
+            print("\n You win" )
+            break
+    
 
 
 
