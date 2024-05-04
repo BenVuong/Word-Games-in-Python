1a528a58ca0ddfb4aed90bb23d33ea4241e725e7
Ben G Vuong <ben.vuong2000@gmail.com>
2024-05-03 19:08:53 -0500

    user can now replay after finishing a game

--- hangman.py
+++ hangman.py
@@ -1,9 +1,5 @@
 from random import randint
-wordList = []
-turns = 8
-unknownWord = []
-listOfWords = []
-listOfLetterGuessed = []
+
 def checkLetter(insertChar, target):
     #takes in the character and check if it is in the target word
     #if it is return an array of all of the indexes of where the character is in the target word
@@ -29,45 +25,78 @@ def checkLetter(insertChar, target):
         print("\n")
 
 
-f = open("wordList.txt", "r")
-for x in f:
-    listOfWords.append(x)
-f.close
-answerIndex = randint(0,len(listOfWords)-1)
-answer=listOfWords[answerIndex]
-answer = answer.strip()
 
-print("Welcome to Hangman")
-print("A random word will be choosen and you will have 8 turns to guess a letter that belongs in that word")
-print("On the 8th turn you will have to type in the entire word")
-for i in range(len(answer)):
-    unknownWord.append("-")
-for i in range(len(unknownWord)):
-    print(unknownWord[i], end=" ")
-for i in range(turns):   
-    print("\n Turn: " + str(i+1) + "\n")
-    if(i == turns-1):
-        
-        userInput = input("Guess the word:")
+end = False
+win = 0
+totalGames = 0
 
-        if(userInput == answer):
-            print("You win")
+while(end == False):
+    wordList = []
+    turns = 8
+    unknownWord = []
+    listOfWords = []
+    listOfLetterGuessed = []
+    f = open("wordList.txt", "r")
+    for x in f:
+        listOfWords.append(x)
+    f.close
+    answerIndex = randint(0,len(listOfWords)-1)
+    answer=listOfWords[answerIndex]
+    answer = answer.strip()
+
+
+    print("\nWelcome to Hangman")
+    print("A random word will be choosen and you will have 8 turns to guess a letter that belongs in that word")
+    print("On the 8th turn you will have to type in the entire word")
+    for i in range(len(answer)):
+        unknownWord.append("-")
+    for i in range(len(unknownWord)):
+        print(unknownWord[i], end=" ")
+    for i in range(turns):   
+        print("\n Turn: " + str(i+1) + "\n")
+        if(i == turns-1):
+            
+            userInput = input("Guess the word:")
+
+            if(userInput == answer):
+                print("You win")
+                win+=1
+                totalGames+=1
+                print("You have won " + str(win) + " out of " + str(totalGames) + " Games")
+                userInput = input("Would you like to play again y/n: ")[0]
+                if (userInput == "n"):
+                    end = True
+            else:
+                print("You lose")
+                print("The word was: " + answer)
+                totalGames+=1
+                print("You have won " + str(win) + " out of " + str(totalGames) + " Games")
+                
+                userInput = input("Would you like to play again y/n: ")[0]
+                if (userInput == "n"):
+                    end = True
         else:
-            print("You lose")
-            print("The word was: " + answer)
-    else:
-        print("Letters that you have already guessed: ", end=" ")
-        for x in listOfLetterGuessed:
-            print(x, end=" ")
-        print("\n")
-        userInput = input('Guess a letter that might be in the word:')[0]
-        listOfLetterGuessed.append(userInput)
-        checkLetter(userInput, answer)
-        final = "".join(unknownWord)
-        if(final == answer):
-            print("\n You win" )
-            break
-    
+            print("Letters that you have already guessed: ", end=" ")
+            for x in listOfLetterGuessed:
+                print(x, end=" ")
+            print("\n")
+            
+            userInput = input('Guess a letter that might be in the word:')[0]
+            listOfLetterGuessed.append(userInput)
+            checkLetter(userInput, answer)
+            
+            final = "".join(unknownWord)
+            if(final == answer):
+                print("\n You win" )
+                win+=1
+                totalGames+=1
+                print("You have won " + str(win) + " out of " + str(totalGames) + " Games")
+                userInput = input("Would you like to play again y/n: ")[0]
+                if (userInput == "n"):
+                    end = True
+                
+                break
+
 
 
 
